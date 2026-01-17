0db4e44e75976860a66a542f9314032e479f3f6c
azimukith <145416259+azimukith@users.noreply.github.com>
2026-01-17 13:47:13 -0700

    Created add job button

--- Uncooked/src/pages/Home.jsx
+++ Uncooked/src/pages/Home.jsx
@@ -1,10 +1,23 @@
-function Home() {
+import { useState } from "react";
+import AddJobModal from "../components/AddJobModal";
+
+export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  const handleSave = (job) => {
+    console.log("Saved job:", job);
+    // later: store to Firebase
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the application!</p>
-    </div>
-  )
-}
+      <button onClick={() => setOpen(true)}>+ Add Job</button>
 
-export default Home
+      <AddJobModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onSave={handleSave}
+      />
+    </div>
+  );
+}
